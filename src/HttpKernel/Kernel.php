a7c62ac9123e498d899ffe4138c51a5435317a80
cx-dev <cxdev@pro-grom.eu>
2017-04-05 12:28:41 +0200

    init

---
+++ src/HttpKernel/Kernel.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the oXylion package.
+ *
+ * (c) Bartosz Zwierzchowski <vedget@pro-grom.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Xylio\HttpKernel;
+
+
+class Kernel implements TerminableInterface
+{
+    public function terminate($request = null, $response = null) {
+        print "hello";
+    }
+}
